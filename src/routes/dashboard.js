14c175150aeb2b2e8eed687e94ee938f4a92f61c
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-11-21 02:30:57 +0530

    sample data upload to db

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -6,57 +6,57 @@ require("dotenv").config();
 const { getReviews, add_feedback, getFeedbacks, updateAnalytics, get_Analytics } = require("../services/reviews");
 
 router.get("/dashboard", async (req, res) => {
-  if (!req.user) {
-    return res.redirect("/auth/login");
-  }
+    if (!req.user) {
+        return res.redirect("/auth/login");
+    }
 
-  const reviews_data = await getReviews();
-  const analytics_data = await get_Analytics();
+    const reviews_data = await getReviews();
+    const analytics_data = await get_Analytics();
 
-  //   reviews_data.map((review) => {
-  //     console.log(review);
-  //   });
+    //   reviews_data.map((review) => {
+    //     console.log(review);
+    //   });
 
-  const user = req.user;
-  res.render("pages/dashboard", { user, reviews_data, analytics_data });
+    const user = req.user;
+    res.render("pages/dashboard", { user, reviews_data, analytics_data });
 });
 
 router.get("/feedback", async (req, res) => {
-  console.log("feedback");
+    console.log("feedback");
 
-  if (!req.user) {
-    return res.redirect("/auth/login");
-  }
+    if (!req.user) {
+        return res.redirect("/auth/login");
+    }
 
-  const user = req.user;
-  const feedback_data = await getFeedbacks();
+    const user = req.user;
+    const feedback_data = await getFeedbacks();
 
-  res.render("pages/feedback", { user, feedback_data });
+    res.render("pages/feedback", { user, feedback_data });
 });
 
 router.post("/feedback_string", async (req, res) => {
-  const feedback_obj = req.body;
+    const feedback_obj = req.body;
 
-  const res_data = await axios.post(process.env.Feedback_API_URL, {
-    text: feedback_obj.feedback,
-  });
-  const data = res_data.data;
-  feedback_obj.sentiment = data.score;
+    const res_data = await axios.post(process.env.Feedback_API_URL, {
+        text: feedback_obj.feedback,
+    });
+    const data = res_data.data;
+    feedback_obj.sentiment = data.score;
 
-  await add_feedback(feedback_obj);
-  await updateAnalytics(feedback_obj.sentiment);
-  res.send("success");
+    await add_feedback(feedback_obj);
+    //await updateAnalytics(feedback_obj.sentiment);
+    res.send("success");
 });
 
 router.post("/getCategory", async (req, res) => {
-  const { text } = req.body;
+    const { text } = req.body;
 
-  const res_data = await axios.post(process.env.Feedback_API_URL, {
-    text,
-  });
-  const data = res_data.data;
+    const res_data = await axios.post(process.env.Feedback_API_URL, {
+        text,
+    });
+    const data = res_data.data;
 
-  res.send(data);
+    res.send(data);
 });
 
 module.exports = router;
